b45db92c190ec9e5c725dedaf9079819f006b062
Gepetto <guillermoezequielperna@gmail.com>
2023-02-28 21:55:13 -0300

    First version. I still need to correct the 50 results limitation.

--- build_dataset.py
+++ build_dataset.py
@@ -1,24 +1,34 @@
 ########################## Libraries ################################################
+import sys
 import requests
 import urllib.parse
 import csv
 ########################## Global_variables #########################################
-site = "MLA"
-search = "tv 4k"
+site = str(sys.argv[1])
+try:
+    search = str(sys.argv[2])
+except:
+    search = "tv 4k"
 search = urllib.parse.quote(search)
 api_url = f"https://api.mercadolibre.com/sites/{site}/search?q={search}"
 ########################## Request ##################################################
-response = requests.get(api_url)
-json = response.json()
-########################## Items_sortation ##########################################
-items_list = []
-for resultado in json["results"]:
-    aux = 0
-    while resultado["attributes"][aux]["id"] != "BRAND":
-        aux += 1
-    items_list.append([resultado["id"], resultado["title"], resultado["price"], resultado["domain_id"], resultado["attributes"][aux]["value_name"]])
+try:
+    response = requests.get(api_url)
+    json = response.json()
+######################### Items_sortation ###########################################
+    items_list = []
+    #print(len(json["results"]))
+    for resultado in json["results"]:
+        if resultado["condition"] == "new":
+            aux = 0
+            while resultado["attributes"][aux]["id"] != "BRAND":
+                aux += 1
+            items_list.append([resultado["id"], resultado["title"], resultado["price"], resultado["domain_id"], resultado["attributes"][aux]["value_name"]])
 ########################## Format_and_exportation ###################################
-with open("dataset.csv","w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(items_list)
-########################## End of script ############################################
+    with open("dataset.csv","w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(items_list)
+########################## Error_handle #############################################
+except:
+    print(site, "no es un sitio válido.")
+########################## End_of_script ############################################
